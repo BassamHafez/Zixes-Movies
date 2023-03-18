033252356052d3a209901623a009f7b9f0fa97f9
Bassam Hafez <bassamhafez790@gmail.com>
2023-03-19 00:14:55 +0200

    make slider home responsive

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,14 +1,35 @@
 import Slider from "react-slick";
-
+import React, {useState,useEffect } from "react";
 export default function SimpleSlider(props) {
+ 
+     
+    let [showNumber,setShowNumber]=useState(6);
 
+    function handleResize() {
+       if(document.body.clientWidth<800){
+        setShowNumber(3);
+       }
+       else if(document.body.clientWidth<1000){
+        setShowNumber(4);
+       }
+
+       else{
+        setShowNumber(6);
+       }
+    }
+   
+    window.addEventListener('resize', handleResize);
+
+    useEffect(()=>{
+      handleResize();
+    },[])
 
     const settings = {
       dots: false,
       infinite: true,
       speed: 2000,
-      slidesToShow: 6,
-      slidesToScroll:6,
+      slidesToShow: showNumber,
+      slidesToScroll:showNumber,
       autoplay:true,
       autoplaySpeed:3000,
       className:'slider-scroll',
