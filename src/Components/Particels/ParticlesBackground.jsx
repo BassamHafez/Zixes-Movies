66c4fe9f8d5b916d4565c6dfdf663a256e3114e3
BassamHafez <bassamhafez790@gmail.com>
2025-04-20 05:18:52 +0200

    deploy

--- src/Components/Particels/ParticlesBackground.jsx
+++ src/Components/Particels/ParticlesBackground.jsx
@@ -1,103 +1,98 @@
-import { useCallback,useContext } from "react";
+import { useCallback, useContext } from "react";
 import { loadFull } from "tsparticles";
 import Particles from "react-tsparticles";
 import NavContext from "../../NavContext";
 
 export default function ParticlesBackground() {
+  let { particleColor } = useContext(NavContext);
 
-    let {particleColor} = useContext(NavContext)
-    
-    
-  const particlesInit = useCallback(async engine => {
+  const particlesInit = useCallback(async (engine) => {
     await loadFull(engine);
-}, []);
+  }, []);
 
-const particlesLoaded = useCallback(async container => {
-}, []);
+  const particlesLoaded = useCallback(async (container) => {}, []);
 
-return (
-  <Particles 
-  id="tsparticles"  
-  init={particlesInit} 
-  loaded={particlesLoaded}
-  options={{
-    fullScreen:{
-      enable:true,
-      zIndex:-1
-    }
-    ,
-    background: {
-        color: {
-            value: "#000",
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fullScreen: {
+          enable: true,
+          zIndex: -1,
         },
-    },
-    fpsLimit: 120,
-    interactivity: {
-        events: {
+        background: {
+          color: {
+            value: "#000",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
             onClick: {
-                enable: true,
-                mode: "push",
+              enable: true,
+              mode: "push",
             },
             onHover: {
-                enable: true,
-                mode: "repulse",
+              enable: true,
+              mode: "repulse",
             },
             resize: true,
-        },
-        modes: {
+          },
+          modes: {
             push: {
-                quantity: 4,
+              quantity: 4,
             },
             repulse: {
-                distance: 200,
-                duration: 0.4,
+              distance: 200,
+              duration: 0.4,
             },
+          },
         },
-    },
-    particles: {
-        color: {
-            value:`${particleColor}`,
-        },
-        links: {
+        particles: {
+          color: {
+            value: `${particleColor}`,
+          },
+          links: {
             color: "#ffffff",
             distance: 150,
             enable: false,
             opacity: 0.5,
             width: 1,
-        },
-        collisions: {
+          },
+          collisions: {
             enable: true,
-        },
-        move: {
+          },
+          move: {
             directions: "none",
             enable: true,
             outModes: {
-                default:"bounce",
+              default: "bounce",
             },
             random: false,
             speed: 1,
             straight: false,
-        },
-        number: {
+          },
+          number: {
             density: {
-                enable: true,
-                area: 800,
+              enable: true,
+              area: 800,
             },
             value: 80,
-        },
-        opacity: {
-            value: .5,
-        },
-        shape: {
-            type:"circle",
-        },
-        size: {
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
             value: { min: 1, max: 5 },
+          },
         },
-    },
-    detectRetina: true,
-}}
-   />
-);
+        detectRetina: true,
+      }}
+    />
+  );
 }
-
